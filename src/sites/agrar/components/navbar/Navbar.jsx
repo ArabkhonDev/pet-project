2c1d6a07523e92a51cd1f70942cce19b8d5d3cf5
ArabkhonDev <starc9899@gmail.com>
2024-05-12 22:26:00 +0500

    navbar  has been completed

--- src/sites/agrar/components/navbar/Navbar.jsx
+++ src/sites/agrar/components/navbar/Navbar.jsx
@@ -1,87 +1,105 @@
+import { useState } from "react";
 import {
   FaFacebookF,
   FaTwitter,
   FaGooglePlusG,
   FaPinterestP,
   FaSearch,
+  FaTimes,
 } from "react-icons/fa";
 
-import {IoMenu} from 'react-icons/io5'
+import { IoMenu } from "react-icons/io5";
 
 import "./navbar.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header>
       <nav>
         <div className="container">
           <div className="nav_main">
             <div className="navbar_logo">
-              <Link to='/'>
-              <span>Agrar Universitet</span>
+              <Link to="/">
+                <span>Agrar Universitet</span>
               </Link>
             </div>
-            <div className="navbar_links">
-              <div className="navbar_links_items">
-                <Link to='/bimm'>
-                <p>BIMM</p>
-                </Link>
-                <Link to='about'>
-                  <p>About</p>
-                </Link>
-                <div class="navbar_links_dropdown">
-                  <button class="dropbtn">Manbalar</button>
-                  <div class="dropdown-content">
-                    <a href="#"> Bosh ilmiy-metodik markaz</a>
-                    <a href="#"> Elektron Kutubxona</a>
-                    <a href="#">Grafik Dizayn</a>
-                    <a href="#">Digital Marketing</a>
+            <div
+              className={isOpen ? "nav_main_links active" : "nav_main_links"}
+            >
+              <div className="navbar_links">
+                <div className="navbar_links_items">
+                  <Link to="/bimm">
+                    <p>BIMM</p>
+                  </Link>
+                  <Link to="about">
+                    <p>About</p>
+                  </Link>
+                  <div class="navbar_links_dropdown">
+                    <button class="dropbtn">Manbalar</button>
+                    <div class="dropdown-content">
+                      <a href="#"> Bosh ilmiy-metodik markaz</a>
+                      <a href="#"> Elektron Kutubxona</a>
+                      <a href="#">Grafik Dizayn</a>
+                      <a href="#">Digital Marketing</a>
+                    </div>
                   </div>
-                </div>
-                <div class="navbar_links_dropdown">
-                  <button class="dropbtn">Narxlar</button>
-                  <div class="dropdown-content">
-                    <a href="#"> Raxbariyat</a>
-                    <a href="#"> Elektron Kutubxona</a>
-                    <a href="#">Grafik Dizayn</a>
-                    <a href="#">Digital Marketing</a>
+                  <div class="navbar_links_dropdown">
+                    <button class="dropbtn">Narxlar</button>
+                    <div class="dropdown-content">
+                      <a href="#"> Raxbariyat</a>
+                      <a href="#"> Elektron Kutubxona</a>
+                      <a href="#">Grafik Dizayn</a>
+                      <a href="#">Digital Marketing</a>
+                    </div>
                   </div>
+                  <Link>
+                    <p>Blog</p>
+                  </Link>
+                  <Link>
+                    <p>Contact</p>
+                  </Link>
                 </div>
-                <p>Blog</p>
-                <p>Contact</p>
+              </div>
+              <div className="navbar_contact_icon_links">
+                <div className="navbar_search_input">
+                  <form action="#" method="get">
+                    <input
+                      type="search"
+                      hidden
+                      name="navbar_search_input"
+                      id=""
+                    />
+                    <FaSearch />
+                    <input type="submit" value="" hidden />
+                  </form>
+                </div>
+                <div className="navbar_link_icons">
+                  <li>
+                    <FaFacebookF />
+                  </li>
+                  <li>
+                    <FaTwitter />
+                  </li>
+                  <li>
+                    <FaGooglePlusG />
+                  </li>
+                  <li>
+                    <FaPinterestP />
+                  </li>
+                </div>
               </div>
             </div>
-            <div className="navbar_contact_icon_links">
-              <div className="navbar_search_input">
-                <form action="#" method="get">
-                  <input
-                    type="search"
-                    hidden
-                    name="navbar_search_input"
-                    id=""
-                  />
-                  <FaSearch />
-                  <input type="submit" value="" hidden />
-                </form>
-              </div>
-              <div className="navbar_link_icons">
-                <li>
-                  <FaFacebookF />
-                </li>
-                <li>
-                  <FaTwitter />
-                </li>
-                <li>
-                  <FaGooglePlusG />
-                </li>
-                <li>
-                  <FaPinterestP />
-                </li>
-              </div>
-            </div>
+
             <div className="menubar">
-              <IoMenu />
+              <div className="navbar-toggle" onClick={toggleMenu}>
+                {isOpen ? <FaTimes /> : <IoMenu />}
+              </div>
             </div>
           </div>
         </div>
--- src/sites/agrar/pages/home/Home.jsx
+++ src/sites/agrar/pages/home/Home.jsx
@@ -3,11 +3,13 @@ import Footer from '../../components/footer/Footer'
 import Navbar from '../../components/navbar/Navbar'
 import MainWorks from '../../components/mainWorks/MainWorks'
 import News from '../../components/news/News'
+import SimpleSlider from '../../components/slider/Slider'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <SimpleSlider />
       <MainWorks />
       <News />
       <Footer />
